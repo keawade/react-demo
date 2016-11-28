81a5c3490252b26bc269ce3c5c2a7e34736d4ce4
Keith Wade <keithwade@nanonation.net>
2016-11-28 17:38:54 -0600

    Initial environment

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.jsx',
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+      },
+      {
+        test: /\.jsx?$/,
+        include: /src/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new StyleLintPlugin({
+      syntax: 'scss',
+      failOnError: false, // Do it.
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+  },
+};
